f27e7975162d90e7e3d5f3a618fabbd83499a2b0
munenelewis <munene2379@gmail.com>
2018-02-10 17:34:58 +0300

    imported credentials in user.py

--- user_test.py
+++ user_test.py
@@ -1,8 +1,12 @@
 import unittest
 from user import User
+from credentials import Credantials
+
 
 class TestUser(unittest.TestCase):
     """Test class that defines test cases for the user class behaviour"""
+
+    
     def setUp(self):
         """run before the test"""
         self.new_user = User("lewis","munene","munenei")#create user object
@@ -39,5 +43,33 @@ class TestUser(unittest.TestCase):
         test_user.save_user()
         self.assertEqual(len(User.user_list),2)
 
+    def test_find_credential(self):
+        '''
+        check if user object is saved into the user list
+        '''
+        self.new_user.save_user()
+        test_user = User('lewis','munene','munenei')
+        test_user.save_user()
+        found_credential = User.find_credential('kim')
+        self.assertEqual(found_credential, False)
+
+    def test_user_exist(self):
+        
+        self.new_user.save_user()
+        test_user = User('lewis','munene','munenei')
+        test_user.save_user()
+
+        user_exists = User.user_exist('lewis')
+        self.assertTrue(user_exists)
+
+    def test_log_in(self):
+        self.new_user.save_user()
+        test_user = User('lewis','munene','munenei')
+        test_user.save_user()
+        found_credential = User.log_in('lewis','munene','munenei')
+        self.assertEqual(found_credential, Credantials.credential_list)
+
+    def test_display_user(self):
+        self.assertEqual( User.display_user() , User.user_list )
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(verbosity=2)
